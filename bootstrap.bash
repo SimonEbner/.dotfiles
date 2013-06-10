8dcdb964a198a2ec57dc700d5332897a81db5a40
Simon Ebner <Simon.Ebner@gmail.com>
2013-06-10 11:35:22 +0200

    Print which file has a backup already

--- bootstrap.bash
+++ bootstrap.bash
@@ -19,7 +19,7 @@ link_files () {
 
         if [ -f $dest ] || [ -d $dest ] || [ -h $dest ]; then
             if [ -f $destBackup ] || [ -d $destBackup ]; then
-                printf "Backup exists already. Remove first"
+                printf "Backup $destBackup exists already. Remove first\n"
                 exit 1
             fi
             printf "Backup of existing file \n"
