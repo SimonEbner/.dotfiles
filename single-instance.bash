3f95dbe7583bc07ae6d71bc5db69ecef6ce8b71d
Simon Ebner <Simon.Ebner@gmail.com>
2013-06-22 12:31:31 +0200

    Improve script

--- single-instance.bash
+++ single-instance.bash
@@ -1,6 +1,6 @@
 #!/bin/zsh
 export VIM_SINGLE=`which vim`
-export SUPPORTS=`vim --version | grep "+clientserver"`
+export SUPPORTS=`$VIM_SINGLE --version | grep "+clientserver"`
 if [ "$SUPPORTS" != '' ];then
     alias vim="single-vim"
 fi
@@ -8,7 +8,7 @@ fi
 function single-vim(){
 echo $VIM_SINGLE
     pid=`ps ax | grep -v grep | grep vim$$` 
-    if [ -z $pid ]; then
+    if [ -z "$pid" ]; then
         $VIM_SINGLE --servername vim$$ $@
     else
         $VIM_SINGLE --servername vim$$ --remote-tab $1
