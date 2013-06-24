71c48f9fe5b0ed0dd2e7f3a35de5883a9c1ef96f
SimonEbner <Simon.Ebner@gmail.com>
2013-06-24 10:55:45 +0200

    Include ~/ba instead of ~/ba/ros to ROS_PKG_PATH

--- bash/ros.bash
+++ bash/ros.bash
@@ -1,6 +1,6 @@
 if [ -f /opt/ros/electric/setup.bash ]; then
     source /opt/ros/electric/setup.bash
 fi
-export ROS_PACKAGE_PATH=~/ba/ros:~/git/care-o-bot:$ROS_PACKAGE_PATH
+export ROS_PACKAGE_PATH=~/ba:~/git/care-o-bot:$ROS_PACKAGE_PATH
 export ROBOT=cob3-6
 export ROBOT_ENV=ipa-kitchen
