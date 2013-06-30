366706bf187a4c75d39319f14dbeee02404d898b
Simon Ebner <Simon.Ebner@gmail.com>
2013-07-01 00:37:56 +0200

    Use . if no dir specified

--- bash/latest.bash
+++ bash/latest.bash
@@ -2,6 +2,7 @@
 function latest(){
     dir=$1
     num=$2
+    if [ -z "$dir" ]; then dir="."; fi
     if [ -z "$num" ]; then num=5; fi
     find $dir -printf '%T+ %p\n' | sort -r | head -n $num
 }
