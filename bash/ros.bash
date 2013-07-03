1ebac1b721df8ed1c83c371cc4dc9bd9cda172eb
SimonEbner <Simon.Ebner@gmail.com>
2013-07-03 13:27:03 +0200

    Helper functions to start rxconsole/rviz when running rostest

--- bash/ros.bash
+++ bash/ros.bash
@@ -4,3 +4,17 @@ fi
 export ROS_PACKAGE_PATH=~/ba:~/git/care-o-bot:$ROS_PACKAGE_PATH
 export ROBOT=cob3-6
 export ROBOT_ENV=ipa-kitchen
+
+function testrxconsole(){
+    uri=$ROS_MASTER_URI
+    export ROS_MASTER_URI=http://localhost:22422
+    rxconsole
+    export ROS_MASTER_URI=$uri
+}
+
+function testrviz(){
+    uri=$ROS_MASTER_URI
+    export ROS_MASTER_URI=http://localhost:22422
+    rosrun rviz rviz
+    export ROS_MASTER_URI=$uri
+}
