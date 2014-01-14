8318094521a36c87800c7056b6270f9ae191de33
Simon Ebner <Simon.Ebner@gmail.com>
2014-01-14 13:32:13 +0100

    Use groovy as default ros distro

--- bash/ros.bash
+++ bash/ros.bash
@@ -1,5 +1,5 @@
-if [ -f /opt/ros/electric/setup.bash ]; then
-    source /opt/ros/electric/setup.bash
+if [ -f /opt/ros/groovy/setup.bash ]; then
+    source /opt/ros/groovy/setup.bash
 fi
 export ROS_PACKAGE_PATH=~/ba:~/git/care-o-bot:$ROS_PACKAGE_PATH
 export ROBOT=cob3-6
