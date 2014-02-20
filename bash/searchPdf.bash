941df7182be6b81562f57581e0076e9dd88301a8
Simon Ebner <Simon.Ebner@gmail.com>
2014-02-20 21:12:09 +0100

    Script to search pdfs

---
+++ bash/searchPdf.bash
@@ -0,0 +1,21 @@
+function searchPdf(){
+    first="$1"
+    second="$2"
+
+    path='.'
+    string=''
+    ignoreCase=''
+    if [ "$1" = "-i" ]; then
+        ignoreCase='-i'
+        first="$2"
+        second="$3"
+    fi
+    if [ -z "$second" ]; then
+        string="$first";
+    else
+        path="$first";
+        string="$second";
+    fi
+    cmd='pdftotext "{}" - | grep '"$ignoreCase"' --with-filename --label="{}" --color '"\"$string\""
+    find $path -name '*.pdf' -exec bash -c "$cmd" \;
+}
