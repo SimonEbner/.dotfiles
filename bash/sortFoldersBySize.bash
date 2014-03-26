0a67f883ef56bc630e37baddb0f7526473067674
Simon Ebner <Simon.Ebner@gmail.com>
2014-03-26 01:47:28 +0100

    Updated script to display results more neatly
    Command found in http://ubuntuforums.org/showthread.php?t=885344&page=2

--- bash/sortFoldersBySize.bash
+++ bash/sortFoldersBySize.bash
@@ -2,5 +2,5 @@
 function sortFoldersBySize(){
     dir=$1
     if [ -z "$dir" ]; then dir="."; fi
-    du --max-depth=1 $dir 2>/dev/null | sort -n -r
+    du --max-depth=0 -k $dir/* | sort -nr | awk '{ if($1>=1024*1024) {size=$1/1024/1024; unit="G"} else if($1>=1024) {size=$1/1024; unit="M"} else {size=$1; unit="K"}; if(size<10) format="%.1f%s"; else format="%.0f%s"; res=sprintf(format,size,unit); printf "%-8s %s\n",res,$2 }'
 }
