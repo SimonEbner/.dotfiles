1d0bbf40f7b217851b49aa162cbb3bd03c22dfc6
Simon Ebner <Simon.Ebner@gmail.com>
2014-04-10 19:49:21 +0200

    Make vim single instance more robust

--- single-instance.bash
+++ single-instance.bash
@@ -5,18 +5,20 @@ if [ "$SUPPORTS" != '' ];then
     alias vim="single-vim"
 fi
 
+export VIM_ID="VIM$RANDOM"
+
 function single-vim(){
     filename=$1
     filenameWithoutDouble=`echo $filename | sed -rn "s/(.*):$/\1/p"`
     if [ ! -f "$filename" ] && [ -f "$filenameWithoutDouble" ]; then
         filename=$filenameWithoutDouble
     fi
-    pid=`ps ax | grep -v grep | grep vim$$` 
+    pid=`$VIM_SINGLE --serverlist | grep $VIM_ID`
     if [ -z "$pid" ]; then
-        $VIM_SINGLE --servername vim$$ $filename
+        $VIM_SINGLE --servername $VIM_ID $filename
     else
-        $VIM_SINGLE --servername vim$$ --remote-tab $filename
-        $VIM_SINGLE --servername vim$$ --remote-send ':tabp<CR>:tabn<CR>'
+        $VIM_SINGLE --servername $VIM_ID --remote-tab $filename
+        $VIM_SINGLE --servername $VIM_ID --remote-send ':tabp<CR>:tabn<CR>'
         fg
     fi
 }
