38b603a99708a681e35dc3e5b5337de94f5d6196
Simon Ebner <Simon.Ebner@gmail.com>
2014-04-27 04:11:25 +0200

    Include scripts to install node and mongo db

---
+++ bash/installNodeAndMongo.bash
@@ -0,0 +1,12 @@
+function installNode(){
+   sudo apt-get install python-software-properties
+   sudo add-apt-repository ppa:chris-lea/node.js
+   sudo apt-get update
+   sudo apt-get install -y nodejs
+}
+function installMongo(){
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+}
