f219b4761f1ababb34de293d1f0f4e2294c6c0c6
Simon Ebner <Simon.Ebner@gmail.com>
2014-05-03 12:08:56 +0200

    Install ruby-dev

--- bash/installScss.bash
+++ bash/installScss.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 function installScss(){
-    sudo apt-get install ruby-full build-essential rubygems
+    sudo apt-get install ruby-full build-essential rubygems, ruby-dev
+    sudo gem install  listen
     sudo gem install sass
     sudo gem install --version '~> 0.9' rb-inotify
 }
