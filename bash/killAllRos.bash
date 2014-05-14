687142daa7fe6fa48014161e5d6bf7b81cd7be27
Simon Ebner <Simon.Ebner@gmail.com>
2014-05-14 14:05:24 +0200

    Include killcode to killAllRos

--- bash/killAllRos.bash
+++ bash/killAllRos.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 function killAllRos(){
-    for i in `ps ax | grep ros | sed -rn "s/([0-9]+).*/\1/p"`; do kill $i; done
+    for i in `ps ax | grep ros | sed -rn "s/([0-9]+).*/\1/p"`; do kill $1 $i; done
 }
