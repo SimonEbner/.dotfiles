0d8c5f5d9432b5c116129cd759d05ce74130e2ec
Simon Ebner <Simon.Ebner@gmail.com>
2014-06-15 13:13:59 +0200

    Add function to build ycm, set e to explore

---
+++ bash/buildYCM.bash
@@ -0,0 +1,9 @@
+function buildYCM(){
+    rm -rf ~/ycm_build
+    mkdir -p ~/ycm_build
+    cd ~/.vim/bundle/YouCompleteMe/
+    ./install.sh
+    cd ~/ycm_build
+    cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+    make ycm_support_libs
+}
