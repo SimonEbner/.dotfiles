35ce86af967f0953774ffd8ea2e646f95395b735
Simon Ebner <Simon.Ebner@gmail.com>
2014-11-19 12:36:33 +0100

    Do an extra sumodlue update

--- bash/buildYCM.bash
+++ bash/buildYCM.bash
@@ -2,6 +2,7 @@ function buildYCM(){
     rm -rf ~/ycm_build
     mkdir -p ~/ycm_build
     cd ~/.vim/bundle/YouCompleteMe/
+    git submodule update --init --recursive
     ./install.sh
     cd ~/ycm_build
     cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
