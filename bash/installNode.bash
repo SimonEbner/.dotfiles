5b29e1548b4ba62034ca00abc3dfc91b8eed774f
Simon Ebner <Simon.Ebner@gmail.com>
2014-12-17 01:53:11 +0100

    Follow http redirects

--- bash/installNode.bash
+++ bash/installNode.bash
@@ -6,7 +6,7 @@ function installNode(){
     curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
     ./configure --prefix=~/local
     make install # ok, fine, this step probably takes more than 30 seconds...
-    curl https://npmjs.org/install.sh | sh
+    curl -L https://npmjs.org/install.sh | sh
     rm -rf ~/node-latest-install
     source ~/.bashrc
 }
