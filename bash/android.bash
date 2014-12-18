435ed283d45d5d917568422698c1e087262c09e4
Simon Ebner <Simon.Ebner@gmail.com>
2014-12-18 11:09:49 +0100

    Android Studio installs into ~/Android

--- bash/android.bash
+++ bash/android.bash
@@ -1,3 +1,6 @@
 if [ -d ~/android-sdks ]; then
     export PATH=~/android-sdks/platform-tools:~/android-sdks/tools:$PATH
 fi
+if [ -d ~/Android/Sdk ]; then
+    export PATH=~/Android/Sdk/platform-tools:~/Android/Sdk/tools:$PATH
+fi
