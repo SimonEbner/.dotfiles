ea1ec143b5ad4183584d66521ae35b8a4548d0cf
Simon Ebner <Simon.Ebner@gmail.com>
2015-06-30 14:12:24 +0200

    Awesome make script

---
+++ bash/mk.bash
@@ -0,0 +1,12 @@
+function mk(){
+    set -o pipefail
+    exec 5>&1
+    FF=$(make 2>&1|tee >(cat - >&5))
+    code=$?
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    if [[ $code == 0 ]]; then
+        echo "NO ERROR"
+    fi
+    ( echo $FF | GREP_COLOR='01;31' egrep --color=always '^([a-zA-Z0-9/._\-]+\.(cpp|h))|$' | GREP_COLOR='01;36' egrep --color=always 'error:(.*)')
+    return $code
+}
