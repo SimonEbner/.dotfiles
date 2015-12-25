d600b6f433898cd34ebe8ad35f695856c67e5889
Simon Ebner <Simon.Ebner@gmail.com>
2015-12-25 16:58:18 +0100

    Multiple updates

--- bash/android.bash
+++ bash/android.bash
@@ -1,5 +1,5 @@
-if [ -d ~/android-sdks ]; then
-    export PATH=~/android-sdks/platform-tools:~/android-sdks/tools:~/android-sdks/build-tools/22.0.1:$PATH
+if [ -d ~/data/android-sdks ]; then
+    export PATH=~/data/android-sdks/platform-tools:~/data/android-sdks/tools:~/data/android-sdks/build-tools/22.0.1:$PATH
 fi
 if [ -d ~/Android/Sdk ]; then
     export PATH=~/Android/Sdk/platform-tools:~/Android/Sdk/tools:~/android-sdks/build-tools/22.0.1:$PATH
